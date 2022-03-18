e5229b7472b31b4397bf1ef3194cd5481f04d963
Fernando <luisfernandosalasg@gmail.com>
2022-03-18 10:29:15 -0500

    Add two stacks using one array

--- 4. Queue/1. contructor.js
+++ 4. Queue/1. contructor.js
@@ -103,7 +103,48 @@ class Queue {
 	}
 }
 
-let myQueue = new Queue();
+class twoStacks {
+	constructor(s) {
+		this.arr = [];
+		this.top1 = -1;
+		this.top2 = s;
+		this.size = s;
+	}
+
+	push1(value) {
+		if (this.top1 < this.top2 - 1) {
+			this.top1 += 1;
+			this.arr[this.top1] = value;
+		}
+	}
+
+	push2(value) {
+		if (this.top1 < this.top2 - 1) {
+			this.top2 -= 1;
+			this.arr[this.top2] = value;
+		}
+	}
+
+	pop1() {
+		if (this.top1 >= 0) {
+			let value = this.arr[this.top1];
+			this.top1 -= 1;
+			return value;
+		} else {
+			return -1;
+		}
+	}
+
+	pop2() {
+		if (this.top2 < this.size) {
+			let val = this.arr[this.top2];
+			this.top2 += 1;
+			return val;
+		} else {
+			return -1;
+		}
+	}
+}
 
 function findBin(number) {
 	let result = [];
@@ -121,7 +162,15 @@ function findBin(number) {
 	return result;
 }
 
-findBin(10);
+let stack = new twoStacks(10);
+stack.push1(20);
+stack.push2(10);
+stack.push1(30);
+stack.push2(40);
+stack.push1(60);
+stack.push2(50);
+
+let myQueue = new Queue();
 
 /* console.log("Enqueue Elements: 2, 4, 6, 8, 10");
 myQueue.enqueue(2);
