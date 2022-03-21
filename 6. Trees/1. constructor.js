14a5653195dac40438fc3501cc950a833192f26a
Fernando <luisfernandosalasg@gmail.com>
2022-03-21 11:11:43 -0500

    Add BinarySearchTree consturctor

--- 6. Trees/1. constructor.js
+++ 6. Trees/1. constructor.js
@@ -13,3 +13,12 @@ myNode.rightChild = new Node(7);
 
 console.log("Value of myNode.leftChild: ", myNode.leftChild.val);
 console.log("Value of myNode.rightChild: ", myNode.rightChild.val);
+
+class BinarySearchTree {
+	constructor(rootValue) {
+		this.root = new Node(rootValue);
+	}
+}
+
+let BST = new BinarySearchTree(8);
+console.log("The root val for BST: ", BST.root.val);
