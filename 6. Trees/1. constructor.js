877b57b52da5abf0d7edd7d42313f7ab4d55202f
Fernando <luisfernandosalasg@gmail.com>
2022-03-22 09:42:35 -0500

    Add insert implementation iterative

--- 6. Trees/1. constructor.js
+++ 6. Trees/1. constructor.js
@@ -6,19 +6,42 @@ class Node {
 	}
 }
 
-let myNode = new Node(6);
+/* let myNode = new Node(6);
 console.log("Value of myNode: ", myNode.val);
 myNode.leftChild = new Node(5);
 myNode.rightChild = new Node(7);
 
 console.log("Value of myNode.leftChild: ", myNode.leftChild.val);
-console.log("Value of myNode.rightChild: ", myNode.rightChild.val);
+console.log("Value of myNode.rightChild: ", myNode.rightChild.val); */
 
 class BinarySearchTree {
 	constructor(rootValue) {
 		this.root = new Node(rootValue);
 	}
+
+	insert(newValue) {
+		if (this.root === null) {
+			this.root = new Node(newValue);
+			return;
+		}
+		//starting from the root
+		let currentNode = this.root;
+		let parent;
+		//while we get to the null node
+		while (currentNode) {
+			//update the parent
+			parent = currentNode;
+			if (newValue < currentNode.val) {
+				//if newValue < parent.val
+				//insert into the leftChild
+				parent.leftChild = new Node(newValue);
+			} else {
+				//if newValue >= parent.val
+				//insert into the rightChild
+				parent.rightChild = new Node(newValue);
+			}
+		}
+	}
 }
 
-let BST = new BinarySearchTree(8);
-console.log("The root val for BST: ", BST.root.val);
+const BST = new BinarySearchTree(8);
