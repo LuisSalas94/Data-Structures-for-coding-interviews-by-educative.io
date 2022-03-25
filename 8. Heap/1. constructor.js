b43c26c20ced81146114fc5033bf4c1f9dfa9616
Fernando <luisfernandosalasg@gmail.com>
2022-03-25 12:26:56 -0500

    Add Max Heap implementation

---
+++ 8. Heap/1. constructor.js
@@ -0,0 +1,14 @@
+class maxHeap {
+	constructor() {
+		this.heap = [];
+		this.elements = 0;
+	}
+
+	insert(val) {}
+	getMax() {}
+	removeMax() {}
+	__percolateUp(index) {}
+	__maxHeapify(index) {}
+}
+
+let heap = new maxHeap();
